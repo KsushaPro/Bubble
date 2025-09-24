e5e577e0c1fb94b7ec8ce1375dacd62191d9c222
Alex Turaev <a.turaev@r77.center-inform.ru>
2025-09-24 20:35:18 +0300

    Метод пузырька

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -4,6 +4,30 @@ package org.example;
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
+        int[] array = {4, 9, 6, 23, 0};
+
+        int max = array[0];
+        for(int i = 0; i<array.length; i++){
+            if (max < array[i]){
+                max = array[i];
+            }
+        }
+        System.out.println(max);
+        System.out.println("-----------");
+
+        for (int i = 0; i < array.length; i++) {
+            for (int j = 0; j < array.length-i-1; j++) {
+                if (array[j] > array[j+1]) {
+                    int temp = array[j];
+                    array[j] = array[j+1];
+                    array[j+1] = temp;
+                }
+            }
+        }
+
+        for (int i = 0; i < array.length; i++) {
+            System.out.println(array[i]);
+        }
     }
 }
 
