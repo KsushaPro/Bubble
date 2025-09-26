3164955becfc82a256c2a177910c3aa3a1d5805a
Alex Turaev <a.turaev@r77.center-inform.ru>
2025-09-26 21:05:23 +0300

    Методы List

--- src/main/java/org/example/service/ListExample.java
+++ src/main/java/org/example/service/ListExample.java
@@ -1,6 +1,7 @@
 package org.example.service;
 
 import java.util.ArrayList;
+import java.util.LinkedList;
 import java.util.List;
 
 public class ListExample {
@@ -25,7 +26,47 @@ public class ListExample {
         listStr.add("Стол");
         listStr.add("Ксюша");
 
-        System.out.println(listStr.get(6));
+        String s1 = "aaa";
+        String s2 = "bbb";
+        String s3 = "ccc";
+
+        List<String> fruits = new ArrayList<String>();
+        fruits.add("Apple");
+        fruits.add("Banana");
+        fruits.add("Cherry");
+
+        listStr.addAll(fruits);
+        listStr.addAll(List.of(s1, s2, s3, s1,s1,s1,s1,s1,s2,s3,s1,s2,s3));
+
+
+        System.out.println(listStr);
+        System.out.println(listStr.size());
+        listStr.remove(5);
+
+        listStr.removeAll( List.of(s1, s2) );
+        System.out.println(listStr);
+        boolean isContains = listStr.contains("Apple");
+        System.out.println("Коллекция listStr содержит строку \"Apple?\": " + isContains);
+        listStr.remove("Apple");
+        isContains = listStr.contains("Apple");
+        System.out.println("Коллекция listStr содержит строку \"Apple?\": " + isContains);
+        listStr.removeAll(List.of("ccc"));
+        System.out.println("Коллекция listStr содержит строку \"ccc\"?: " + listStr.contains("ccc"));
+
+
+        ListService listService = new ListService();
+        List<String> resultStrings = listService
+                .of("aaa")
+                .of("bbb", "ccc")
+                .of("a", "b", "c")
+                .of(fruits)
+                .getResult();
+
+        System.out.println(resultStrings);
+
+        // Частеы МЕТОДЫ LIST: add, size, remove, isEmpty, contains,toArray, containsAll, addAll,
+        // removeAll, retain, sort, sublist ит.д.
+
 
 
     }
---
+++ src/main/java/org/example/service/ListService.java
@@ -0,0 +1,42 @@
+package org.example.service;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ListService {
+
+    private List<String> list = new ArrayList<>();
+
+    public ListService of(String a) {
+        list.add(a);
+        return this;
+    }
+
+    public  ListService of(String a, String b) {
+        list.addAll(List.of(a, b));
+        return this;
+    }
+
+    public  ListService of(String a, String b, String c) {
+        list.addAll(List.of(a, b, c));
+        return this;
+    }
+
+    public ListService of(List<String> l) {
+        this.list.addAll(l);
+        return this;
+    }
+
+    public List<String> getResult() {
+        return this.list;
+    }
+
+
+    public List<String> getList() {
+        return list;
+    }
+
+    public void setList(List<String> list) {
+        this.list = list;
+    }
+}
